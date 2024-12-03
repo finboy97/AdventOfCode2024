74e0d858bf9ad70605fec1d438b89b15cf7a1844
finboy <finbar201@gmail.com>
2024-12-03 16:34:11 +0000

    updated comment

--- source/03/toboggan_company_computer.py
+++ source/03/toboggan_company_computer.py
@@ -2,7 +2,6 @@ import re
 
 regex_str = r"mul\([0-9]{1,6},[0-9]{1,6}\)"
 multiply_regex = re.compile(regex_str)
-#xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
 
 def multiply_line(line):
     matches = multiply_regex.findall(line)
@@ -16,7 +15,10 @@ def multiply_line(line):
 
 
 def part_1():
-    """Expected to equal 161"""
+    """
+    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+    Expected to equal 161
+    """
     with open("input1.txt") as f:
         total = 0
         for line in f.readlines():
@@ -25,7 +27,9 @@ def part_1():
 
 
 def part_2():
-    """expected to equal 48"""
+    """
+    #xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+    expected to equal 48"""
     with open("input1.txt") as f:
 
         total_2 = 0
